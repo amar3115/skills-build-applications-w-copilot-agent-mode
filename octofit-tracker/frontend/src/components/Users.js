c573c4bf81bfb395826e002d6b73195a6470cb66
amar3115 <uakskillup@gmail.com>
2025-04-08 17:53:13 +0000

    fron end code added

---
+++ octofit-tracker/frontend/src/components/Users.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://redesigned-space-telegram-q766prgv55p72xvvv-8000.app.github.dev/api/users/')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Users</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
